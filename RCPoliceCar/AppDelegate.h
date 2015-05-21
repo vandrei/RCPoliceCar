39dba98a8ddf454a3cb82f02348bea1ba9ef910c
Andrei Vasilescu <andrei.vasilescu@mready.ro>
2015-05-21 17:42:51 +0300

    created project

---
+++ RCPoliceCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RCPoliceCar
+//
+//  Created by Andrei Vasilescu on 21/05/15.
+//  Copyright (c) 2015 mready. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
